abce266c185ab7da3a354a24d7291436019934d4
Marcos Rodriguez <marcos.rodriguez@clarcat.com>
2018-07-20 12:16:27 +0200

    ESLint and babel config

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import next from 'next';
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
