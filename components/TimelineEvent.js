212e3c8f22eed001015955979b7ff23de8db912d
Marcos Rodriguez <marcos.rodriguez@clarcat.com>
2018-08-07 09:08:20 +0200

    Some workaround on Timeline (style and content)

--- components/TimelineEvent.js
+++ components/TimelineEvent.js
@@ -1,50 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TimelineEvent = ({ date, sub, title, company, location, children }) => (
-  // <li className="event" data-date={date} data-sub={sub}>
-  //   <h3>{title}</h3>
-  //   <h4>{company}</h4>
-  //   {children}
-  //   <div className="flex">
-  //     <p className="location">
-  //       <span className="icon fa-map-marker spaced" />
-  //       {location}
-  //     </p>
-  //   </div>
-  // </li>
-
+const TimelineEvent = ({ date, sub, techs, title, company, location, children }) => (
   <div className="timeline-event-container">
     <div className="timeline-event-date">
-      <div className="timeline-event-year">{date}</div>
-      <div className="timeline-event-sub">{sub}</div>
+      <div className="timeline-event-year" title="From - to">{date}</div>
+      <div className="timeline-event-sub" title="Total time">{sub}</div>
     </div>
     <div className="timeline-event-description">
-      <div className="dot" />
-      <h3>{title}</h3>
-      <h4>{company}</h4>
-      {children}
-      <div className="flex">
-        <p className="location">
-          <span className="icon fa-map-marker spaced" />
+      <div className="dot" title="Career checkpoint! (Red means 'current' :P)" />
+      <h3 title="Job title">{title}</h3>
+      <h4 title="Company">{company}</h4>
+      <div className="flex flex-column">
+        <p className="event-description-item location">
+          <span title="Location" className="icon fa-map-marker spaced" />
           {location}
         </p>
-        <p className="date">
-          <span className="icon fa-calendar spaced" />
+        <p className="event-description-item date">
+          <span title="From - to / total time" className="icon fa-calendar spaced" />
           {date}
         </p>
+        <p className="event-description-item techs">
+          <span title="Technologies summary" className="icon fa-cogs spaced" />
+          {techs}
+        </p>
       </div>
+      {children}
     </div>
   </div>
 );
 
 TimelineEvent.propTypes = {
   date: PropTypes.string.isRequired,
-  sub: PropTypes.string.isRequired,
+  sub: PropTypes.string,
   title: PropTypes.string.isRequired,
+  techs: PropTypes.string,
   company: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+};
+
+TimelineEvent.defaultProps = {
+  techs: "",
+  sub: "",
 };
 
 export default TimelineEvent;
--- pages/Home.js
+++ pages/Home.js
@@ -7,7 +7,6 @@ import Card from '../components/Card';
 import Cards from '../components/Cards';
 import Actions from '../components/Actions';
 import Banner from '../components/Banner';
-import Professional from '../components/Professional';
 import Timeline from '../components/Timeline';
 import TimelineEvent from '../components/TimelineEvent';
 
@@ -56,6 +55,7 @@ class Home extends React.PureComponent {
                   title="Fullstack Senior Software Engineer"
                   company="Grupo Clarcat"
                   location="Gijón / Xixón"
+                  techs="AngularJS | Angular6 | PHP | ASP .Net | MariaDB"
                 >
                   <p>
                     Sed posuere consectetur est at lobortis. Nullam quis risus
@@ -70,6 +70,7 @@ class Home extends React.PureComponent {
                   company="BinPar Digital Ignition"
                   location="Madrid"
                   date="2017 - 2018"
+                  techs="React | Redux | GraphQL | Apollo | NodeJS | MongoDB"                  
                 >
                   <p>
                     Sed posuere consectetur est at lobortis. Nullam quis risus
@@ -82,8 +83,9 @@ class Home extends React.PureComponent {
                 <TimelineEvent
                   title="Fullstack ASP.NET Software Engineer"
                   company="iBakari Desarrollos Informáticos"
-                  location="Oviedo"
+                  location="Oviedo / Uviéu"
                   date="2015 - 2017"
+                  techs="ASP .Net MVC5 | EntityFramework | SQLServer | SQLServerCE | AngularJS"
                 >
                   <p>
                     Sed posuere consectetur est at lobortis. Nullam quis risus
@@ -96,8 +98,9 @@ class Home extends React.PureComponent {
                 <TimelineEvent
                   title="Fullstack Software Engineer / Robotics Engineer"
                   company="Adele Robots"
-                  location="Oviedo"
+                  location="Oviedo / Uviéu"
                   date="2014 - 2015"
+                  techs="Spring | AngularJS | PHP | Slim Framework | TitanDB | MySQL"
                 >
                   <p>
                     Sed posuere consectetur est at lobortis. Nullam quis risus
@@ -110,7 +113,7 @@ class Home extends React.PureComponent {
                 <TimelineEvent
                   title="Java FX and Javascript Developer"
                   company="Seresco Soluciones"
-                  location="Oviedo"
+                  location="Oviedo / Uviéu"
                   date="2014"
                   sub="( 3 mos. )"
                 >
@@ -125,7 +128,7 @@ class Home extends React.PureComponent {
                 <TimelineEvent
                   title="ASPX Developer"
                   company="ASAC Comunicaciones"
-                  location="Llanera, Oviedo"
+                  location="Oviedo / Uviéu"
                   date="2010"
                   sub="( 4 mos. )"
                 >
